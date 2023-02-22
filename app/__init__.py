9d03d25fe57a83674b15f0db5fc4b2fdcfc5057c
uky-syauki <achmad.uky@gmail.com>
2023-02-22 18:02:18 +0000

    New Update v.11

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask-bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel
 
 
 app = Flask(__name__)
@@ -18,6 +20,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
 	if app.config['MAIL_SERVER']:
@@ -43,5 +47,9 @@ if not app.debug:
 	app.logger.setLevel(logging.INFO)
 	app.logger.info('Microblog startup')
 
+
+@babel.localeselector
+def get_locale():
+	return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 from app import routes, models
--- config.py
+++ config.py
@@ -13,3 +13,4 @@ class Config(object):
 	MAIL_PASSWORD = "nhfeeiuutruywcrz"  #os.environ.get('MAIL_PASSWORD')
 	ADMINS = ['info.blog.uki@gmail.com']
 	POSTS_PER_PAGE = 25
+	LANGUAGES = ['en', 'ind']
